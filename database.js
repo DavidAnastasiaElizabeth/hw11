750cbcbd1f6a5205862a27d5c2d6cf690ef423cf
David Woolf <davidjohnwoolf@gmail.com>
2015-10-04 15:42:38 -0700

    create database file

---
+++ database.js
@@ -0,0 +1,28 @@
+var database = {};
+
+var database.users = [
+	{username: ''},
+	{username:'Anastasia'},
+	{username:'Elizabeth'},
+	{username:'David'}
+];
+
+var database.posts = [
+  { title:'Twitter is okay...TEAM DAE TWITTER is AmAzInG',
+    author:    'David'
+  },
+  { title:'Twitter should honstly consider a rebuild based on this model',
+    author:    'Anastasia'
+  },
+  { title:'Team DAE Twitter rules',
+    author:    'Elizabeth'
+  },
+  { title:'Team Dae is going to participate in a clone-a-thon',
+    author:    'David'
+  },
+  { title:'Twitter...more like DAEitter',
+    author:    'Anastasia'
+  }
+];
+
+module.exports = database;
