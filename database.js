21b3682a858306461bdd7e8247ae9013d336681a
ELIZABETH <elizabethkdeering@gmail.com>
2015-10-04 16:21:28 -0700

    updated database with info

--- database.js
+++ database.js
@@ -2,26 +2,26 @@ var database = {};
 
 database.users = [
 	{username: ''},
-	{username:'Anastasia'},
-	{username:'Elizabeth'},
-	{username:'David'}
+	{username:'Anastasia', password:'doglady', bio:'dog lover, pizza eater.'},
+	{username:'Elizabeth', password:'catlady', bio:'cat lover, cookie eater.'},
+	{username:'David', password:'one', bio:'dragon lover, gangsta.'}
 ];
 
 database.posts = [
   { title:'Twitter is okay...TEAM DAE TWITTER is AmAzInG',
-    author:    'David'
+    author:'David'
   },
   { title:'Twitter should honstly consider a rebuild based on this model',
-    author:    'Anastasia'
+    author:'Anastasia'
   },
   { title:'Team DAE Twitter rules',
-    author:    'Elizabeth'
+    author:'Elizabeth'
   },
   { title:'Team Dae is going to participate in a clone-a-thon',
-    author:    'David'
+    author:'David'
   },
   { title:'Twitter...more like DAEitter',
-    author:    'Anastasia'
+    author:'Anastasia'
   }
 ];
 
