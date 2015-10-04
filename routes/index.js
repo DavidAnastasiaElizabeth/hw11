5bf5ac8d7fe966a25d3756dc52768284cb115d0c
ELIZABETH <elizabethkdeering@gmail.com>
2015-10-04 16:51:55 -0700

    validated username and password

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log(req.body.username);
+  for(var i = 0; i<database.users.length; i++){
+    if(database.users[i].username === req.body.username && database.users[i].password === req.body.password){
+        res.render('main', { title: 'Hermit App', user: database.users[i]});
+      }
+  }
+  alert('that is wrong and you suck.');
 });
 
 
