691741350248e6c17430af07c24f752b7403c5d0
ELIZABETH <elizabethkdeering@gmail.com>
2015-10-04 16:14:05 -0700

    added POST/ route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var database = require('../database');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login to Hermit' });
 });
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.username);
+});
+
 
 module.exports = router;
