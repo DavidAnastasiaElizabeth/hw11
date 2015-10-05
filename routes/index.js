054b08cd74f2794f05a6acc54b5133eecc48285a
David Woolf <davidjohnwoolf@gmail.com>
2015-10-05 16:09:18 -0700

    add registration routes

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,49 @@ var express = require('express');
 var database = require('../database');
 var router = express.Router();
 
-/* GET home page. */
+// Login Routes
+// ---------------
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login to Hermit' });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body.username);
   for(var i = 0; i<database.users.length; i++){
-    if(database.users[i].username === req.body.username && database.users[i].password === req.body.password){
+    if (database.users[i].username === req.body.username && database.users[i].password === req.body.password){
         res.render('main', { title: 'Hermit App', user: database.users[i]});
       }
   }
-  alert('that is wrong and you suck.');
+  console.log('Username or password incorrect');
+  res.render('login', { title: 'Login to Hermit' });
 });
 
+// Register Routes
+// ---------------
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Sign Up' });
+});
+
+router.post('/users', function(req, res, next) {
+  var usernameAvailable = true;
+  for (var i = 0; i < database.users.length; i++) {
+    if (req.body.username === database.users[i].username) {
+      usernameAvailable = false;
+    }
+  }
+  if (usernameAvailable) {
+    var user = {
+      username: req.body.username,
+      password: req.body.password,
+      bio: req.body.bio
+    }
+
+    database.users.push(user);
+    res.redirect('/');
+  } else {
+    console.log('Username taken');
+    res.redirect('/register');
+  }
+
+});
 
 module.exports = router;
