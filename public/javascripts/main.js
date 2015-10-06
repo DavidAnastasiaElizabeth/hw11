df700d7e3862f1ead38f1f8d96197eae7a32225c
Anastasia Bennett <anastasia.n.bennett@gmail.com>
2015-10-05 19:44:06 -0700

    Appended the CreatePostView to UserView

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,9 @@ var hermit = {};
 
 var userView = new UserView();
 $(document.body).append(userView.render().$el);
+
+var createPostView = new CreatePostView();
+$('#user-view').append(createPostView.render());
+
+var recentPostsView = new RecentPostsView();
+$('#user-view').append(recentPostsView.render());
