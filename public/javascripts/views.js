d65d3b44bef3e813ce9f4814099652f95f924a44
Anastasia Bennett <anastasia.n.bennett@gmail.com>
2015-10-11 16:37:56 -0700

    Chirps

--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -31,7 +31,8 @@ var HermitViews = (function() {
     className: 'post-view',
     render: function() {
       this.listenTo(this.collection, 'update', this.render);
-      var postEdit = '<textarea id="post-Edit"></textarea>';
+      var titleEditTxt = '<textarea id="title-Edit"></textarea>';
+      var contentEditTxt = '<textarea id="post-Edit"></textarea>';
       var titleContent = $('#title').html();
       var bodyContent = $('#content').html();
       var title = '<h4 id="title">' + this.model.get('title') + '</h4>';
@@ -53,10 +54,17 @@ var HermitViews = (function() {
       });
       var editTitle = '<input type="text" value=' + this.model.get('title') + '>';
       var editContent = '<input type="text" value=' + this.model.get('content') + '>';
+
       this.collection.each(function(post) {
-        var postView = new PostView({ model: post });
-        this.$el.append(postView.render().$el);
+        var revisedTitle = new title({ model: post });
+        this.$el.append(revisedTitle.render().$el);
       }, this);
+
+      this.collection.each(function(post) {
+        var revisedContent  = new content({ model: post });
+        this.$el.append(revisedContent.render().$el);
+      }, this);
+
       return this;
     }
   });
@@ -84,8 +92,8 @@ var EditView = Backbone.View.extend({
       var createpostViewContainer = '<div id="createpostViewContainer">';
       var postTitle = '<input type="text" id="post-title">';
       var postBody = '<textarea id="post-body"></textarea>';
-      var savePost = '<button id="savepost">Post</button>';
-      this.$el.html(createpostViewContainer + '<br><div>' + postTitle + '</div><div>' + postBody + '</div><div>' + savePost + '</div>');
+      var savePost = '<button id="savepost">Chirp</button>';
+      this.$el.html(createpostViewContainer + '<br><div>' + 'Chrip Title' + '</div>' + postTitle + '</div><div>' + '<br><div>' + 'Chrip' + '</div>' + postBody + '</div><div>' + savePost + '</div>');
       return this;
     },
 
@@ -115,7 +123,7 @@ var EditView = Backbone.View.extend({
     },
 
     render: function() {
-      var label = '<h2>Recent Posts</h2>';
+      var label = '<h2>Recent Chirps</h2>';
       this.$el.html(label);
       this.collection.each(function(post) {
         var postView = new PostView({ model: post });
@@ -132,7 +140,7 @@ var EditView = Backbone.View.extend({
     },
 
     render: function() {
-      var label = '<h2>Users Posts</h2>';
+      var label = '<h2>User Chirps</h2>';
       this.$el.html(label);
       this.collection.each(function(post) {
         var postView = new PostView({ model: post });
